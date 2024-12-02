a11c45d4b4df1c8d23f60887322042abdba7abfd
ivan Alejandro Noriega Llerena <noriegallerenaivanalejandro@gmail.com>
2024-12-02 14:15:34 -0500

    actualizacion del driver de conexcion a bd

--- assets/model/conexcionDataBase.php
+++ assets/model/conexcionDataBase.php
@@ -2,7 +2,7 @@
     class conection{
         static public function conectar(){
             try{
-                $conn = new PDO("mysql:host=localhost;dbname=scm", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                $conn = new PDO("mysql:host=bg5hkgpf7xqkv4sukieo-mysql.services.clever-cloud.com;dbname=bg5hkgpf7xqkv4sukieo", "ufmob2qfxcjv2h7y", "CrvkjMmwSBlHCmaspRKy", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 return $conn;
             }
             catch(PDOException $e){
